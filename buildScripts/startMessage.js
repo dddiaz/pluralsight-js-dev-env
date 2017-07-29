c55f0c06e7aa111cb663db041aae246ac67939b8
Daniel Diaz <daniel.delvin.diaz@gmail.com>
2017-07-28 23:18:12 -0700

    babel set up

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,6 @@
+// This is es5 syntax
+//var chalk = require('chalk');
+//This is es6 which is transpiled with babel
+import chalk from 'chalk';
+
+console.log(chalk.green('Starting app in dev mode...'))
