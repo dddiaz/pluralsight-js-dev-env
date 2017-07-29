4c0510a9a4cd1155c766051c23ea754b07b287a9
Daniel Diaz <daniel.delvin.diaz@gmail.com>
2017-07-29 00:45:09 -0700

    testing set up

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -3,4 +3,4 @@
 //This is es6 which is transpiled with babel
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in dev mode...'))
+console.log(chalk.green('Starting app in dev mode...')); //eslint-disable-line no-console
